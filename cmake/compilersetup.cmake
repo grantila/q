111faf78f99fd858afac2ff4a66896f5af0f9e9b
Gustaf Räntilä <g.rantila@gmail.com>
2017-01-28 18:18:14 +0100

    Builds on Ubuntu (wrt linking)

--- cmake/compilersetup.cmake
+++ cmake/compilersetup.cmake
@@ -3,7 +3,7 @@ if ( NOT CMAKE_CXX_STANDARD )
 	set( CMAKE_CXX_STANDARD 11 )
 endif ( )
 
-SET ( CMAKE_CXX_STANDARD ${CMAKE_CXX_STANDARD} CACHE STRING
+set( CMAKE_CXX_STANDARD ${CMAKE_CXX_STANDARD} CACHE STRING
 	"C++ standard to use, currently 11 or 14."
 	FORCE )
 
@@ -15,34 +15,36 @@ set( CMAKE_CXX_EXTENSIONS OFF )
 if ( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
 
 	if ( NOT CMAKE_CXX_FLAGS )
-		SET ( CMAKE_CXX_FLAGS "-Wall  -Wextra -pipe -pthread" )
+		set( CMAKE_CXX_FLAGS "-Wall -Wextra -pipe -pthread" )
 
-		SET ( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
+		set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
+			"Flags used by the compiler during all build types."
+			FORCE )
+	endif ( )
+
+	if ( NOT CMAKE_EXE_LINKER_FLAGS )
+		set( CMAKE_EXE_LINKER_FLAGS "-ldl -lpthread" )
+		# The above seems not to work, while manually adding them works:
+		set( GENERIC_LIB_DEPS dl pthread )
+		set( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} CACHE STRING
+			"Flags used by the linker during the creation of modules."
+			FORCE )
+	endif ( )
+
+elseif ( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
+
+	if ( NOT CMAKE_CXX_FLAGS )
+		# use clang std, this enables using of llvm on old platforms, like RHEL6
+		set( CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++ -Wall -pipe -pthread" )
+
+		set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
 			"Flags used by the compiler during all build types."
 			FORCE )
 		endif ( )
 
-		if ( NOT CMAKE_EXE_LINKER_FLAGS )
-			SET ( CMAKE_EXE_LINKER_FLAGS "-ldl -lpthread" )
-			SET ( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} CACHE STRING
-				"Flags used by the linker during the creation of modules."
-				FORCE )
-		endif ( )
-
-	elseif ( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
-
-		if ( NOT CMAKE_CXX_FLAGS )
-			# use clang std, this enables using of llvm on old platforms, like RHEL6
-			SET ( CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++ -Wall -pipe -pthread" )
-
-			SET ( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
-				"Flags used by the compiler during all build types."
-				FORCE )
-			endif ( )
-
 	if ( NOT CMAKE_EXE_LINKER_FLAGS )
-		SET ( CMAKE_EXE_LINKER_FLAGS "-lc++ -lc++abi -ldl -lpthread" )
-		SET ( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} CACHE STRING
+		set( CMAKE_EXE_LINKER_FLAGS "-lc++ -lc++abi -ldl -lpthread" )
+		set( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} CACHE STRING
 			"Flags used by the linker during the creation of modules."
 			FORCE )
 	endif ( )
@@ -51,9 +53,9 @@ elseif ( CMAKE_CXX_COMPILER_ID MATCHES "AppleClang" )
 
 	if ( NOT CMAKE_CXX_FLAGS )
 		# recommand this
-		SET ( CMAKE_CXX_FLAGS "-Wall -pipe -pthread" )
+		set( CMAKE_CXX_FLAGS "-Wall -pipe -pthread" )
 
-		SET ( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
+		set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
 			"Flags used by the compiler during all build types."
 			FORCE )
 	endif ( )
